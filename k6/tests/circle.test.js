b72f674b57806fb1e9d9f2236796a6f88fdde6ee
SHresTho12 <talimul12bari@gmail.com>
2023-09-25 13:02:48 +0600

    connection fixewd to the outside app

--- k6/tests/circle.test.js
+++ k6/tests/circle.test.js
@@ -4,7 +4,7 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export const options = {
-  vus: 1, // Key for Smoke test. Keep it at 2, 3, max 5 VUs
+  vus: 3, // Key for Smoke test. Keep it at 2, 3, max 5 VUs
   duration: "1m", // This can be shorter or just a few iterations
 };
 
